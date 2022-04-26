7a6bf28a600869bd2ba1203ebb7bf53cb4413da9
Georgia Lumley <georgia.lumley@hotmail.com>
2022-04-26 19:25:37 +0100

    heightlight values if incorrect

--- src/components/Sudoku/Cell.tsx
+++ src/components/Sudoku/Cell.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { decodeCellPosition } from "../../utils";
-import miracleSudoku from "../../sudokus/miracleSudoku.json";
+import miracleSudokuJson from "../../sudokus/miracleSudoku.json";
 
-import { Container, CellValue } from "./Sudoku.styles";
+import { CellValue } from "./Sudoku.styles";
 
 interface IProps {
   position: string;
@@ -17,19 +17,38 @@ interface Sudoku {
   };
 }
 const Cell = ({ position }: IProps) => {
+  const miracleSudoku: Sudoku = miracleSudokuJson;
+  const [sudoku, updateSudoku] = useState(miracleSudoku);
   const cellPosition = decodeCellPosition(position);
 
   const isGridEdge = Number(cellPosition.column) % 3 === 0;
   const isGridBottom = Number(cellPosition.row) % 3 === 0;
 
-  const sudoku: Sudoku = miracleSudoku;
-  console.log(sudoku[position], { position });
   return (
-    <Container isGridEdge={isGridEdge} isGridBottom={isGridBottom}>
-      <CellValue isStartingValue={!!sudoku[position].startingValue}>
-        {sudoku[position].startingValue}
-      </CellValue>
-    </Container>
+    <CellValue
+      isGridEdge={isGridEdge}
+      isGridBottom={isGridBottom}
+      isStartingValue={!!sudoku[position].startingValue}
+      name={position}
+      type="text"
+      maxLength={1}
+      min={1}
+      max={9}
+      value={sudoku[position].startingValue || sudoku[position].value || ""}
+      incorrectValue={
+        !!sudoku[position].value &&
+        sudoku[position].value !== sudoku[position].answer
+      }
+      onChange={(e) => {
+        updateSudoku({
+          ...sudoku,
+          [position]: {
+            ...sudoku[position],
+            value: Number(e.target.value),
+          },
+        });
+      }}
+    ></CellValue>
   );
 };
 export default Cell;
--- src/components/Sudoku/Sudoku.styles.ts
+++ src/components/Sudoku/Sudoku.styles.ts
@@ -24,8 +24,27 @@ export const GridRow = styled.div`
   display: flex;
 `;
 
-export const CellValue = styled.p<{ isStartingValue: boolean }>`
+export const CellValue = styled.input<{
+  isStartingValue: boolean;
+  isGridEdge: boolean;
+  isGridBottom: boolean;
+  incorrectValue: boolean;
+}>`
   margin: 0;
   font-size: 25px;
   font-weight: ${({ isStartingValue }) => (isStartingValue ? "600" : "400")};
+  outline: none;
+  width: 64px;
+  height: 64px;
+  border: 1px solid black;
+  border-right: ${({ isGridEdge }) =>
+    isGridEdge ? "2px solid black" : "1px solid black"};
+  border-bottom: ${({ isGridBottom }) =>
+    isGridBottom ? "2px solid black" : "1px solid black"};
+  align-items: center;
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  -webkit-appearance: none;
+  color: ${({ incorrectValue }) => (incorrectValue ? "red" : "black")};
 `;
